77376095dc5d588cdd734bfa5922b1615f552005
Vinay Patel <vinayspatel0@gmail.com>
2025-02-25 13:15:28 +0000

    refactor: remove unused imports

--- app/src/main/java/com/example/composearticlekotlin/MainActivity.kt
+++ app/src/main/java/com/example/composearticlekotlin/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -18,7 +17,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composearticlekotlin.ui.theme.ComposeArticleKotlinTheme
