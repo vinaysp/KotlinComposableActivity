486cd9bdc3da450595caa270098c5204a75a7eb4
Vinay Patel <vinayspatel0@gmail.com>
2025-02-25 12:27:46 +0000

    feat: import stringResource

--- app/src/main/java/com/example/composearticlekotlin/MainActivity.kt
+++ app/src/main/java/com/example/composearticlekotlin/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
